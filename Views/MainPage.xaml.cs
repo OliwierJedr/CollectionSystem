8b941058772daffbda7e4e39860dede8caf90e8d
OliwierJedr <160551845+OliwierJedr@users.noreply.github.com>
2024-04-15 22:15:35 +0200

    MainPage.xaml.cs

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using CollectionManagmentSystem.Models;
+using CollectionManagmentSystem.Models;
 using CollectionManagmentSystem.Controls;
 using CollectionManagmentSystem.Services;
 using CollectionManagmentSystem.Views;
@@ -16,9 +16,6 @@ namespace CollectionManagmentSystem
             InitializeComponent();
             Directory.CreateDirectory(_dataFolderPath);
             LoadCollections();
-
-            
-
         }
 
         private void LoadCollections()
@@ -51,25 +48,27 @@ namespace CollectionManagmentSystem
                 File.Create(filePath).Dispose();
 
                 _collections.Add(new Collection { Id = _collections.Count + 1, CollectionName = collectionName });
-                collectionListView.ItemsSource = null;
-                collectionListView.ItemsSource = _collections;
+                LoadCollections();
+                RefreshCollectionListView();
             }
         }
+        private void RefreshCollectionListView() => collectionListView.ItemsSource = _collections;
+
 
         private async void OnDeleteCollection_Clicked(object sender, EventArgs e)
         {
             if (collectionListView.SelectedItem is Collection selectedCollection)
             {
                 var confirm = await DisplayAlert("Delete Collection", $"Are you sure you want to delete collection: {selectedCollection.CollectionName}", "Yes", "No");
-
                 if (confirm)
                 {
                     var filePath = Path.Combine(_dataFolderPath, $"{selectedCollection.CollectionName}.txt");
                     File.Delete(filePath);
 
                     _collections.Remove(selectedCollection);
-                    collectionListView.ItemsSource = null;
-                    collectionListView.ItemsSource = _collections;
+                    
+                    RefreshCollectionListView();
+                    LoadCollections();
                 }
             }
             else
@@ -81,49 +80,69 @@ namespace CollectionManagmentSystem
 
         private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(e.CurrentSelection.FirstOrDefault() is Collection selectedCollection)
-            {
-                descriptionLayout1.IsVisible = true;
-                descriptionLayout1.BindingContext = selectedCollection;
-                descriptionLayout2.IsVisible = true;
-                descriptionLayout2.BindingContext = selectedCollection;
+            if (e.CurrentSelection.FirstOrDefault() is Collection selectedCollection) ShowCollectionDescription(selectedCollection);
+            else HideCollectionDescription();
+        }
 
-            }
-            else
-            {
-                descriptionLayout1.IsVisible = false;
-                descriptionLayout2.IsVisible = false;
-            }
+        private void ShowCollectionDescription(Collection selectedCollection)
+        {
+            descriptionLayout1.IsVisible = true;
+            descriptionLayout1.BindingContext = selectedCollection;
+            descriptionLayout2.IsVisible = true;
+            descriptionLayout2.BindingContext = selectedCollection;
+        }
+
+        private void HideCollectionDescription()
+        {
+            descriptionLayout1.IsVisible = false;
+            descriptionLayout2.IsVisible = false;
         }
 
         public async void OnEditCollection_Clicked(object sender, EventArgs e)
         {
-            if(sender is Button button && button.BindingContext is Collection selectedCollection)
+            if (sender is Button button && button.BindingContext is Collection selectedCollection)
             {
-                string newName = await DisplayPromptAsync("Edit Collection Name", "Enter new name: ", "OK", "Cancel", selectedCollection.CollectionName);
+                string newName = await GetNewCollectionName(selectedCollection);
                 if (!string.IsNullOrWhiteSpace(newName))
                 {
-                    var oldFilePath = Path.Combine(_dataFolderPath, $"{selectedCollection.CollectionName}.txt");
-                    var newFilePath = Path.Combine(_dataFolderPath, $"{newName}.txt");
-                    if (File.Exists(oldFilePath))
-                    {
-                        File.Move(oldFilePath, newFilePath);
-                        File.Delete(oldFilePath);
-                    }
-                    selectedCollection.CollectionName = newName;
-                    collectionListView.ItemsSource = null;
-                    collectionListView.ItemsSource = _collections;
+                    UpdateCollectionFilePath(selectedCollection, newName);
+                    UpdateCollectionName(selectedCollection, newName);
                     LoadCollections();
                 }
             }
         }
 
+        private async Task<string> GetNewCollectionName(Collection selectedCollection)
+        {
+            return await DisplayPromptAsync("Edit Collection Name", "Enter new name: ", "OK", "Cancel", selectedCollection.CollectionName);
+        }
+
+        private void UpdateCollectionFilePath(Collection selectedCollection, string newName)
+        {
+            string oldFilePath = GetCollectionFilePath(selectedCollection.CollectionName);
+            string newFilePath = GetCollectionFilePath(newName);
+
+            if (File.Exists(oldFilePath))
+            {
+                File.Move(oldFilePath, newFilePath);
+                File.Delete(oldFilePath);
+            }
+        }
+
+        private void UpdateCollectionName(Collection selectedCollection, string newName)
+        {
+            selectedCollection.CollectionName = newName;
+        }
+
+        private string GetCollectionFilePath(string collectionName)
+        {
+            return Path.Combine(_dataFolderPath, $"{collectionName}.txt");
+        }
+
         private async void OnViewCollection_Clicked(object sender, EventArgs e)
         {
             if(sender is Button button && button.BindingContext is Collection selectedCollection)
-            {
                 await Navigation.PushAsync(new CollectionPage(selectedCollection));
-            }
         }
 
         private void ShowDataPath_Clicked(object sender, EventArgs e)
@@ -131,11 +150,8 @@ namespace CollectionManagmentSystem
             dataPathLabel.IsVisible = !dataPathLabel.IsVisible;
 
             if (dataPathLabel.IsVisible)
-            {
                 dataPathLabel.Text = $"Data is stored in: {_dataFolderPath}";
-            }
         }
-
 
     }
 }
