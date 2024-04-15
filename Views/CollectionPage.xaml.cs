4b281f78bcd6fa118e98e992976438a7d673fc30
OliwierJedr <160551845+OliwierJedr@users.noreply.github.com>
2024-04-15 22:13:22 +0200

    CollectionPage.xaml.cs

--- Views/CollectionPage.xaml.cs
+++ Views/CollectionPage.xaml.cs
@@ -2,6 +2,8 @@ using CollectionManagmentSystem.Views;
 using CollectionManagmentSystem.Models;
 using CollectionManagmentSystem.Controls;
 using CollectionManagmentSystem.Services;
+using System.Diagnostics;
+
 
 namespace CollectionManagmentSystem.Views;
 
@@ -36,74 +38,32 @@ public partial class CollectionPage : ContentPage
     private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (e.CurrentSelection.FirstOrDefault() is CollectionItem selectedItem)
-        {
             editButtonLayout.IsVisible = true;
-        }
         else
-        {
             editButtonLayout.IsVisible = false;
-        }
     }
 
     private async void OnEdit_Clicked(object sender, EventArgs e)
     {
         if (collectionView.SelectedItem is CollectionItem selectedItem)
         {
-            
             string selectedProperty = await DisplayActionSheet("Select property to edit:", "Cancel", null, "Item Name", "Price", "Rating", "Status");
             if (selectedProperty == null || selectedProperty == "Cancel")
-                return; 
+                return;
 
-            string newValue;
             switch (selectedProperty)
             {
                 case "Item Name":
-                    newValue = await DisplayPromptAsync("Edit Item Name", "Enter new item name:", "OK", "Cancel", selectedItem.ItemName);
-                    if (newValue == null) {
-                        return;
-                    }
-                        
-                    selectedItem.ItemName = newValue;
+                    await EditItemName(selectedItem);
                     break;
                 case "Price":
-                    newValue = await DisplayPromptAsync("Edit Price", "Enter new price:", "OK", "Cancel", selectedItem.Price.ToString());
-                    if (newValue == null)
-                    {
-                        return;
-                    }
-                        
-                    double newPrice;
-                    if (!double.TryParse(newValue, out newPrice))
-                    {
-                        await DisplayAlert("Error", "Invalid price. Please enter a valid number.", "OK");
-                        return;
-                    }
-
-                    selectedItem.Price = newPrice;
+                    await EditPrice(selectedItem);
                     break;
                 case "Rating":
-                    newValue = await DisplayPromptAsync("Edit Rating", "Enter new rating(1-10):", "OK", "Cancel", selectedItem.Rating.ToString());
-                    if (newValue == null)
-                    {
-                        return;
-                    }
-                        
-                    int newRating;
-                    if (!int.TryParse(newValue, out newRating))
-                    {
-                        await DisplayAlert("Error", "Invalid rating. Please enter a valid number.", "OK");
-                        return;
-                    }
-                    selectedItem.Rating = newRating;
+                    await EditRating(selectedItem);
                     break;
                 case "Status":
-                    newValue = await GetStatusPrompt();
-                    if (newValue == null)
-                    {
-                        return;
-                    }
-                         
-                    selectedItem.Status = newValue;
+                    await EditStatus(selectedItem);
                     break;
             }
 
@@ -114,66 +74,132 @@ public partial class CollectionPage : ContentPage
         }
     }
 
+    private async Task EditItemName(CollectionItem selectedItem)
+    {
+        string newValue = await DisplayPromptAsync("Edit Item Name", "Enter new item name:", "OK", "Cancel", selectedItem.ItemName);
+        if (newValue != null)
+            if (ItemNameAlreadyExists(newValue))
+                await ConfirmAddingNewItem();
+            else
+                selectedItem.ItemName = newValue;
+    }
+
+    private async Task EditPrice(CollectionItem selectedItem)
+    {
+        string newValue = await DisplayPromptAsync("Edit Price", "Enter new price:", "OK", "Cancel", selectedItem.Price.ToString());
+        if (newValue != null)
+        {
+            double newPrice;
+            if (double.TryParse(newValue, out newPrice))
+                selectedItem.Price = newPrice;
+            else
+                await DisplayAlert("Error", "Invalid price. Please enter a valid number.", "OK");
+        }
+    }
+
+    private async Task EditRating(CollectionItem selectedItem)
+    {
+        string newValue = await DisplayPromptAsync("Edit Rating", "Enter new rating (1-10):", "OK", "Cancel", selectedItem.Rating.ToString());
+        if (newValue != null)
+        {
+            int newRating;
+            if (int.TryParse(newValue, out newRating) && newRating >= 1 && newRating <= 10)
+                selectedItem.Rating = newRating;
+            else
+                await DisplayAlert("Error", "Invalid rating. Please enter a value between 1 and 10.", "OK");
+        }
+    }
+
+    private async Task EditStatus(CollectionItem selectedItem)
+    {
+        string newValue = await GetStatusPrompt();
+        if (newValue != null)
+            selectedItem.Status = newValue;
+    }
+
     private async void OnAdd_Clicked(object sender, EventArgs e)
     {
-        string itemName = await DisplayPromptAsync("New Item", "Enter your item's name: ");
-        if (itemName == null)
+        string itemName = await PromptForItemName();
+        if (itemName != null)
         {
-            return;
-        }
-
-        if (_collectionItems.Any(item => item.ItemName == itemName))
-        {
-            var confirm = await DisplayAlert("Warn", "An item with provided name already exists. Do you want to add new one?", "Yes", "No");
-            if (!confirm)
+            if (ItemNameAlreadyExists(itemName))
             {
-                return;
+                await ConfirmAddingNewItem();
+                return; 
+            }
+
+            double price = await PromptForPrice();
+            if (price != -1) 
+            {
+                int rating = await PromptForRating();
+                if (rating != -1) 
+                {
+                    string status = await PromptForStatus();
+                    if (status != null)
+                        AddNewItem(itemName, price, rating, status);
+                }
             }
         }
+    }
 
+    private async Task<string> PromptForItemName()
+    {
+        return await DisplayPromptAsync("New Item", "Enter your item's name: ");
+    }
+
+    private bool ItemNameAlreadyExists(string itemName)
+    {
+        return _collectionItems.Any(item => item.ItemName == itemName);
+    }
+
+    private async Task<bool> ConfirmAddingNewItem()
+    {
+        await DisplayAlert("Warning", "An item with provided name already exists. Please choose a different name.", "OK");
+        return true;
+    }
+
+    private async Task<double> PromptForPrice()
+    {
         double price;
-        while (true)
+        string priceText;
+        do
         {
-            string priceText = await DisplayPromptAsync("New Item", "Enter your item's price(PLN): ");
+            priceText = await DisplayPromptAsync("New Item", "Enter your item's price (PLN): ");
             if (priceText == null)
-            {
-                return;
-            }
+                return -1; 
 
-            if (double.TryParse(priceText, out price))
-            {
-                break;
-            }
+            if (!double.TryParse(priceText, out price))
+                await DisplayAlert("Error", "Invalid price. Please enter a valid value", "OK");
+        } while (!double.TryParse(priceText, out price));
 
-            await DisplayAlert("Error", "Invalid price. Please enter valid value", "OK");
-        }
+        return price;
+    }
 
+    private async Task<int> PromptForRating()
+    {
         int rating;
-        while (true)
+        string ratingText;
+        do
         {
-            string ratingText = await DisplayPromptAsync("New Item", "Rate your item(1-10): ");
+            ratingText = await DisplayPromptAsync("New Item", "Rate your item (1-10): ");
             if (ratingText == null)
-            {
-                return;
-            }
+                return -1;
 
-            if (int.TryParse(ratingText, out rating))
-            {
-                break;
-            }
-                
-            await DisplayAlert("Error", "Invalid rating. Please enter valid value", "OK");
-        }
+            if (!int.TryParse(ratingText, out rating) || rating < 1 || rating > 10)
+                await DisplayAlert("Error", "Invalid rating. Please enter a value between 1 and 10.", "OK");
+        } while (!int.TryParse(ratingText, out rating) || rating < 1 || rating > 10);
 
-        string status = await GetStatusPrompt();
-        if(status == null)
-        {
-            return;
-        }
+        return rating;
+    }
 
+    private async Task<string> PromptForStatus()
+    {
+        return await GetStatusPrompt();
+    }
 
+    private void AddNewItem(string itemName, double price, int rating, string status)
+    {
         int newItemId = _collectionItems.Count + 1;
-
         CollectionItem newItem = new CollectionItem
         {
             ItemId = newItemId,
@@ -181,16 +207,15 @@ public partial class CollectionPage : ContentPage
             Price = price,
             Rating = rating,
             Status = status
-        }; 
-
+        };
         _collectionItems.Add(newItem);
 
         var dataService = new FileDataService();
-
         dataService.SaveCollectionItems(_collection.CollectionName, _collectionItems);
-
         LoadCollectionItems();
     }
+
+
 
     private void OnDelete_Clicked(object sender, EventArgs e)
     {
