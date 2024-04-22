914aa40a82aa20b2a3101a8a802b4f84fd5f52f0
OliwierJedr <160551845+OliwierJedr@users.noreply.github.com>
2024-04-22 18:53:43 +0200

    MainPage.xaml.cs

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -2,12 +2,12 @@ using CollectionManagmentSystem.Models;
 using CollectionManagmentSystem.Controls;
 using CollectionManagmentSystem.Services;
 using CollectionManagmentSystem.Views;
-
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 namespace CollectionManagmentSystem
 {
     public partial class MainPage : ContentPage
     {
-        private readonly FileDataService _fileDataService = new FileDataService();
         private readonly string _dataFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Collections");
         private List<Collection> _collections;
 
@@ -15,7 +15,7 @@ namespace CollectionManagmentSystem
         {
             InitializeComponent();
             Directory.CreateDirectory(_dataFolderPath);
-            Debug.WriteLine("Data stored in: " + _dataFolderPath);
+            Debug.WriteLine("DATA STORED IN: " + _dataFolderPath + "\n");
             LoadCollections();
         }
 
@@ -39,22 +39,40 @@ namespace CollectionManagmentSystem
 
             if (!string.IsNullOrWhiteSpace(collectionName))
             {
-                var filePath = Path.Combine(_dataFolderPath, $"{collectionName}.txt");
-                if (File.Exists(filePath))
+                if (!IsValidCollectionName(collectionName))
                 {
-                    await DisplayAlert("Warning", "A collection with the same name already exists. Please choose a different name.", "OK");
+                    await DisplayAlert("Error", "Collection name cannot contain special characters.", "OK");
                     return;
                 }
 
-                File.Create(filePath).Dispose();
-
-                _collections.Add(new Collection { Id = _collections.Count + 1, CollectionName = collectionName });
-                LoadCollections();
-                RefreshCollectionListView();
+                AddCollection(collectionName);
             }
         }
-        private void RefreshCollectionListView() => collectionListView.ItemsSource = _collections;
 
+        private bool IsValidCollectionName(string collectionName)
+        {
+            return !ContainsSpecialCharacters(collectionName);
+        }
+
+        private async void AddCollection(string collectionName)
+        {
+            string formattedCollectionName = RemoveSpecialCharacters(collectionName);
+
+            formattedCollectionName = formattedCollectionName.Replace(" ", "_");
+
+            var fileName = $"{formattedCollectionName}.txt";
+            var filePath = Path.Combine(_dataFolderPath, fileName);
+            if (File.Exists(filePath))
+            {
+                await DisplayAlert("Warning", "A collection with the same name already exists. Please choose a different name.", "OK");
+                return;
+            }
+
+            File.Create(filePath).Dispose();
+
+            _collections.Add(new Collection { Id = _collections.Count + 1, CollectionName = collectionName });
+            LoadCollections();
+        }
 
         private async void OnDeleteCollection_Clicked(object sender, EventArgs e)
         {
@@ -65,38 +83,15 @@ namespace CollectionManagmentSystem
                 {
                     var filePath = Path.Combine(_dataFolderPath, $"{selectedCollection.CollectionName}.txt");
                     File.Delete(filePath);
-
                     _collections.Remove(selectedCollection);
-                    
-                    RefreshCollectionListView();
+
+                    collectionListView.ItemsSource = _collections;
                     LoadCollections();
                 }
             }
             else
-            {
                 await DisplayAlert("Error", "Please select a collection to delete", "OK");
-            }
-        }
-
-
-        private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.CurrentSelection.FirstOrDefault() is Collection selectedCollection) ShowCollectionDescription(selectedCollection);
-            else HideCollectionDescription();
-        }
-
-        private void ShowCollectionDescription(Collection selectedCollection)
-        {
-            descriptionLayout1.IsVisible = true;
-            descriptionLayout1.BindingContext = selectedCollection;
-            descriptionLayout2.IsVisible = true;
-            descriptionLayout2.BindingContext = selectedCollection;
-        }
-
-        private void HideCollectionDescription()
-        {
-            descriptionLayout1.IsVisible = false;
-            descriptionLayout2.IsVisible = false;
+            
         }
 
         public async void OnEditCollection_Clicked(object sender, EventArgs e)
@@ -135,16 +130,48 @@ namespace CollectionManagmentSystem
             selectedCollection.CollectionName = newName;
         }
 
-        private string GetCollectionFilePath(string collectionName)
-        {
-            return Path.Combine(_dataFolderPath, $"{collectionName}.txt");
-        }
-
         private async void OnViewCollection_Clicked(object sender, EventArgs e)
         {
-            if(sender is Button button && button.BindingContext is Collection selectedCollection)
+            if (sender is Button button && button.BindingContext is Collection selectedCollection)
                 await Navigation.PushAsync(new CollectionPage(selectedCollection));
         }
 
+        private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is Collection selectedCollection) ShowCollectionDescription(selectedCollection);
+            else HideCollectionDescription();
+        }
+
+        private void ShowCollectionDescription(Collection selectedCollection)
+        {
+            descriptionLayout1.IsVisible = true;
+            descriptionLayout1.BindingContext = selectedCollection;
+            descriptionLayout2.IsVisible = true;
+            descriptionLayout2.BindingContext = selectedCollection;
+        }
+
+        private void HideCollectionDescription()
+        {
+            descriptionLayout1.IsVisible = false;
+            descriptionLayout2.IsVisible = false;
+        }
+
+        private string GetCollectionFilePath(string collectionName)
+        {
+            string formattedCollectionName = collectionName.Replace(" ", "_");
+            return Path.Combine(_dataFolderPath, $"{formattedCollectionName}.txt");
+        }
+
+        private bool ContainsSpecialCharacters(string input)
+        {
+            string pattern = "[^a-zA-Z0-9 ]";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        private string RemoveSpecialCharacters(string input)
+        {
+            string pattern = "[^a-zA-Z0-9 ]";
+            return Regex.Replace(input, pattern, "");
+        }
     }
 }
